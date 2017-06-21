c8dc788e6825b9dd3d00ac929fff1eaa980891cf
Jason DeTiberus <jdetiber@redhat.com>
2017-06-21 13:06:49 -0400

    add some auto-detection

---
+++ roles/kvm_network/filter_plugins/kvm_network_filters.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# vim: expandtab:tabstop=4:shiftwidth=4
+'''
+Custom filters for use in kvm_network role
+'''
+
+
+VIRBR_REGEX = re.compile(r'virbr(\d+)')
+ONE_NINE_TWO_REGEX = re.compile(r'192.168.(\d+).\d')
+
+def first_available_virbr(ansible_interfaces):
+    dev_nums = [int(re.match(dev).group(1)) for dev in ansible_interfaces if re.match(dev) is not None]
+    if dev_nums:
+        return 'virbr{}'.format(max(dev_nums) + 1)
+    else:
+        return 'virbr0'
+
+
+def next_available_cidr(ansible_all_ipv4_addresses):
+    one_nine_twos = [int.(re.match(net).group(1)) for net in ansible_all_ipv4_addresses if re.match(net) is not None]
+    if one_nine_twos:
+        return '192.168.{}.0/24'.format(max(one_nine_twos) + 1)
+    else:
+        return '192.168.133.0/24'
+
+
+class FilterModule(object):
+    ''' Custom ansible filters for use by the openshift_master role'''
+
+    def filters(self):
+        ''' returns a mapping of filters to methods '''
+        return {
+            "first_available_virbr": first_available_virbr,
+            "next_available_cidr": next_available_cidr
+        }
